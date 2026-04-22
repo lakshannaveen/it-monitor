1099cf54436eb6520719ef89beac7f5da3351286
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2026-04-22 08:29:07 +0530

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectStats, selectGroupedByJobType, selectAllRecords, selectLoading, selectError } from "../store/selectors";
 import { useFetch } from "../hooks/useFetch";
-import JobTypeChart from "../components/features/dashboard/JobTypeChart";
 import EmployeeStrip from "../components/features/dashboard/EmployeeStrip";
 import LoadingState from "./states/LoadingState";
 import ErrorState from "./states/ErrorState";
@@ -10,7 +9,6 @@ import ErrorState from "./states/ErrorState";
 const DashboardPage = () => {
   const { refetch } = useFetch();
   const stats = useSelector(selectStats);
-  const grouped = useSelector(selectGroupedByJobType);
   const records = useSelector(selectAllRecords);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
@@ -22,10 +20,6 @@ const DashboardPage = () => {
     <div className="animate-fade-in">
       {/* Section A – Employee Strip */}
       <EmployeeStrip records={records} stats={stats} />
-
-      <div className="mt-4">
-        <JobTypeChart grouped={grouped} />
-      </div>
     </div>
   );
 };
